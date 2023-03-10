be5add9ae5a1ecd84a4a1bf963ee1edfcf314f31
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2023-03-10 19:13:57 +0330

    Update CommitTransactionFailureException.php

--- src/Throwable/CommitTransactionFailureException.php
+++ src/Throwable/CommitTransactionFailureException.php
@@ -6,7 +6,7 @@
 
 namespace Delight\Db\Throwable;
 
-/** Exception that is thrown when a transaction cannot be committed successfully for some reason */
+
 class CommitTransactionFailureException extends TransactionFailureException {}
 
 ?>
