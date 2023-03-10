47ca20c4cc6373f7355df1847c5a91502cd840c3
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2023-03-10 19:13:46 +0330

    Update BeginTransactionFailureException.php

--- src/Throwable/BeginTransactionFailureException.php
+++ src/Throwable/BeginTransactionFailureException.php
@@ -6,7 +6,7 @@
 
 namespace Delight\Db\Throwable;
 
-/** Exception that is thrown when a transaction cannot be started successfully for some reason */
+
 class BeginTransactionFailureException extends TransactionFailureException {}
 
 ?>
